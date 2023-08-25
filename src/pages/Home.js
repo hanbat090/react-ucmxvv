00b04f90d4e236eb1f0216dd28fcf320b88e7c8a
pamtrg2606 <hanbat090@gmail.com>
2023-08-25 19:55:05 +0700

    c

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,388 @@ import { useNavigate } from 'react-router-dom';
 function Home({ login }) {
   let navigate = useNavigate();
   return (
-    <div>
-      <p>This is the Home Page</p>
-      <button onClick={()=>{
-        login();
-        navigate('/dashboard')
-      }}>
-        Login
-      </button>
-      {/* {location.state && <p>From the About Page</p>} */}
+    <div className="index">
+      <div className="div">
+        <div className="group">
+          <div className="group-2">
+            <div className="text-wrapper">IT LAND</div>
+            <div className="text-wrapper-2">About Us</div>
+            <div className="text-wrapper-3">Services</div>
+            <div className="text-wrapper-4">Contact Us</div>
+            <div className="text-wrapper-5">Follow Us</div>
+            <div className="text-wrapper-6">Help</div>
+            <div className="text-wrapper-7">Privacy Policy</div>
+            <div className="text-wrapper-8">About Company</div>
+            <div className="text-wrapper-9">Vacancies</div>
+            <div className="text-wrapper-10">Employers</div>
+            <div className="text-wrapper-11">Courses</div>
+            <div className="text-wrapper-12">Company</div>
+            <div className="text-wrapper-13">Freelance</div>
+            <div className="text-wrapper-14">Contact Us</div>
+            <div className="text-wrapper-15">Help</div>
+            <div className="text-wrapper-16">Help</div>
+            <div className="text-wrapper-17">Help</div>
+            <p className="p">(+84 ) 123 - 456 - 789</p>
+            <img
+              className="facebook-logo"
+              alt="Facebook logo"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/facebook-logo-240--1-@2x.png"
+            />
+            <img
+              className="img"
+              alt="Facebook logo"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/facebook-logo-240--1--1@2x.png"
+            />
+            <img
+              className="facebook-logo-2"
+              alt="Facebook logo"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/facebook-logo-240--1--2@2x.png"
+            />
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group">
+                <div className="overlap">
+                  <div className="text-wrapper-18">Sign Up</div>
+                </div>
+                <div className="text-wrapper-19">Email Address</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-2">
+          <div className="overlap-wrapper">
+            <div className="overlap-3">
+              <img
+                className="image"
+                alt="Image"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/image-2.png"
+              />
+              <div className="rectangle" />
+            </div>
+          </div>
+          <div className="div-wrapper">
+            <div className="overlap-4">
+              <div className="rectangle-2" />
+              <div className="rectangle-3" />
+              <div className="text-wrapper-20">Search</div>
+              <img
+                className="search-regular"
+                alt="Search regular"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/search-regular-240--1--1@2x.png"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="overlap-5">
+          <div className="group-3">
+            <div className="group-4">
+              <div className="overlap-group-2">
+                <div className="group-5" />
+                <div className="text-wrapper-21">
+                  Exceptkjkdfksjdfhdhfkdfjkdjfksdfsfhskdfskfskdfskdfnksjfnkjnfksjfnksdfnskdmfns,msdsjbdjsbdsndfmsnfmsfnsmdfnfdbsdffsbdfm,snda,mnsd,amd,amnd,mds,assjdbsjdsjdfnjsndkasndmsnfmsdfnmsdfnmsnfsmnfmsdnfmsnfmsnfmsnfmsnfsdfjsjfsjdnfsjfsdfsdfsfsdfsdfsdfsdfsfasfasfasdasdasdfafsafsafsdfsdfsdfsdfdsafkjabfkafnamn,sfns,dfns,ndfExceptkjkdfksjdfhdhfkdfjkdjfksdfsfhskdfskfskdfskdfnksjfnkjnfksjfnksdfnskdmfns,msdsjbdjsbdsndfmsnfmsfnsmdfnfdbsdffsbdfm,snda,mnsd,amd,amnd,mds,assjdbsjdsjdfnjsndkasndmsnfmsdfnmsdfnmsnfsmnfmsdnfmsnfmsnfmsnfmsnfsdfjsjfsjdnfsjfsdfsdfsfsdfsdfsdfsdfsfasfasfasdasdasdfafsafsafsdfsdfsdfsdfdsafkjabfkafnamn,sfns,dfns,ndfdfsdfsfasfasfasdasdasdfafsafsafsdfcsdfdsafkjabfkafnamn,sfns,dfns,ndfsdfasdadsdasdasdasd.
+                </div>
+                <div className="overlap-6">
+                  <div className="text-wrapper-22">Registration For Courses</div>
+                </div>
+                <div className="overlap-7">
+                  <div className="text-wrapper-23">Add Course</div>
+                </div>
+              </div>
+            </div>
+            <div className="text-wrapper-24">Courses</div>
+          </div>
+          <div className="group-6">
+            <div className="group-7">
+              <div className="rectangle-4" />
+              <div className="rectangle-5" />
+            </div>
+            <div className="group-8">
+              <div className="rectangle-4" />
+              <div className="rectangle-6" />
+            </div>
+          </div>
+        </div>
+        <div className="overlap-8">
+          <div className="group-9">
+            <div className="overlap-9">
+              <div className="text-wrapper-25">IT LAND</div>
+            </div>
+          </div>
+          <div className="group-10">
+            <div className="text-wrapper-26">Home</div>
+            <div className="text-wrapper-27">Internships</div>
+            <div className="text-wrapper-28">Courses</div>
+          </div>
+          <img
+            className="line"
+            alt="Line"
+            src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/line-16.svg"
+          />
+          <div className="group-11">
+            <img
+              className="chat-regular"
+              alt="Chat regular"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/chat-regular-240@2x.png"
+            />
+            <img
+              className="bookmark-regular"
+              alt="Bookmark regular"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/bookmark-regular-240@2x.png"
+            />
+            <div className="ellipse" />
+          </div>
+          <div className="text-wrapper-29">V</div>
+        </div>
+        <div className="overlap-10">
+          <div className="group-12">
+            <div className="overlap-11">
+              <div className="group-wrapper">
+                <div className="group-13">
+                  <div className="group-14">
+                    <div className="overlap-group-3">
+                      <div className="overlap-12">
+                        <div className="text-wrapper-30">WordPress Development</div>
+                        <div className="text-wrapper-31">Mercedes - Benz</div>
+                      </div>
+                      <div className="text-wrapper-32">View Details</div>
+                      <img
+                        className="line-2"
+                        alt="Line"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/line-2.svg"
+                      />
+                      <img
+                        className="line-3"
+                        alt="Line"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/line-3.svg"
+                      />
+                      <img
+                        className="bookmark-regular-2"
+                        alt="Bookmark regular"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/bookmark-regular-240-1@2x.png"
+                      />
+                      <img
+                        className="logo-zalo-arc"
+                        alt="Logo zalo arc"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/logo-zalo-arc-1@2x.png"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="group-15">
+                <div className="text-wrapper-33">6 Months</div>
+                <div className="text-wrapper-34">3tr /month</div>
+                <div className="group-16" />
+                <div className="group-17" />
+                <div className="group-18">
+                  <div className="text-wrapper-35">Ha Noi</div>
+                </div>
+                <div className="location-icon">
+                  <div className="ellipse-wrapper">
+                    <div className="ellipse-2" />
+                  </div>
+                </div>
+              </div>
+              <img
+                className="bookmark-regular-3"
+                alt="Bookmark regular"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/bookmark-regular-240-2@2x.png"
+              />
+            </div>
+            <div className="group-19">
+              <div className="overlap-13">
+                <div className="text-wrapper-36">See all internships</div>
+              </div>
+            </div>
+            <div className="text-wrapper-37">Top Internships</div>
+            <div className="overlap-14">
+              <div className="group-20">
+                <div className="overlap-group-3">
+                  <div className="overlap-group-4">
+                    <div className="text-wrapper-30">PhP Development</div>
+                    <div className="text-wrapper-31">Google</div>
+                  </div>
+                  <div className="text-wrapper-32">View Details</div>
+                  <img
+                    className="line-2"
+                    alt="Line"
+                    src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/line-2.svg"
+                  />
+                  <img
+                    className="line-3"
+                    alt="Line"
+                    src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/line-3.svg"
+                  />
+                  <img
+                    className="bookmark-regular-2"
+                    alt="Bookmark regular"
+                    src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/bookmark-regular-240-1@2x.png"
+                  />
+                </div>
+              </div>
+              <div className="group-15">
+                <div className="text-wrapper-33">6 Months</div>
+                <div className="text-wrapper-34">3tr /month</div>
+                <div className="group-16" />
+                <div className="group-17" />
+                <div className="group-18">
+                  <div className="text-wrapper-35">Ha Noi</div>
+                </div>
+                <div className="location-icon">
+                  <div className="ellipse-wrapper">
+                    <div className="ellipse-2" />
+                  </div>
+                </div>
+              </div>
+              <img
+                className="bookmark-regular-3"
+                alt="Bookmark regular"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/bookmark-regular-240-2@2x.png"
+              />
+              <div className="group-21" />
+            </div>
+            <div className="overlap-15">
+              <div className="group-20">
+                <div className="overlap-group-3">
+                  <div className="overlap-group-5">
+                    <div className="text-wrapper-30">3D Design</div>
+                    <div className="text-wrapper-31">Vin Group</div>
+                  </div>
+                  <div className="text-wrapper-32">View Details</div>
+                  <img
+                    className="line-2"
+                    alt="Line"
+                    src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/line-2.svg"
+                  />
+                  <img
+                    className="line-3"
+                    alt="Line"
+                    src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/line-3.svg"
+                  />
+                  <img
+                    className="bookmark-regular-2"
+                    alt="Bookmark regular"
+                    src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/bookmark-regular-240-1@2x.png"
+                  />
+                </div>
+              </div>
+              <div className="group-22">
+                <div className="text-wrapper-33">6 Months</div>
+                <div className="text-wrapper-34">3tr /month</div>
+                <div className="group-16" />
+                <div className="group-17" />
+                <div className="group-23">
+                  <div className="text-wrapper-35">Ho Chi Minh</div>
+                </div>
+                <div className="location-icon">
+                  <div className="ellipse-wrapper">
+                    <div className="ellipse-2" />
+                  </div>
+                </div>
+              </div>
+              <img
+                className="bookmark-regular-3"
+                alt="Bookmark regular"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/bookmark-regular-240-2@2x.png"
+              />
+              <img
+                className="logo-zalo-arc-2"
+                alt="Logo zalo arc"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/logo-zalo-arc-10@2x.png"
+              />
+            </div>
+            <div className="overlap-16">
+              <div className="group-20">
+                <div className="overlap-group-3">
+                  <div className="overlap-group-6">
+                    <div className="text-wrapper-30">Unity Game Development</div>
+                    <div className="text-wrapper-31">FPT Software</div>
+                  </div>
+                  <div className="text-wrapper-32">View Details</div>
+                  <img
+                    className="line-2"
+                    alt="Line"
+                    src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/line-2.svg"
+                  />
+                  <img
+                    className="line-3"
+                    alt="Line"
+                    src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/line-3.svg"
+                  />
+                  <img
+                    className="bookmark-regular-2"
+                    alt="Bookmark regular"
+                    src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/bookmark-regular-240-1@2x.png"
+                  />
+                </div>
+              </div>
+              <div className="group-15">
+                <div className="text-wrapper-33">6 Months</div>
+                <div className="text-wrapper-34">3tr /month</div>
+                <div className="group-16" />
+                <div className="group-17" />
+                <div className="group-18">
+                  <div className="text-wrapper-35">Ha Noi</div>
+                </div>
+                <div className="location-icon">
+                  <div className="ellipse-wrapper">
+                    <div className="ellipse-2" />
+                  </div>
+                </div>
+              </div>
+              <img
+                className="bookmark-regular-3"
+                alt="Bookmark regular"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/bookmark-regular-240-2@2x.png"
+              />
+              <img
+                className="image-alt-regular"
+                alt="Image alt regular"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/image-alt-regular-240-2@2x.png"
+              />
+            </div>
+          </div>
+          <div className="group-24">
+            <div className="group-7">
+              <div className="rectangle-4" />
+              <div className="rectangle-7" />
+            </div>
+            <div className="group-8">
+              <div className="rectangle-4" />
+              <div className="rectangle-6" />
+            </div>
+          </div>
+        </div>
+        <div className="group-25">
+          <div className="group-26">
+            <img
+              className="image-alt-regular-2"
+              alt="Image alt regular"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/image-alt-regular-240-1@2x.png"
+            />
+            <img
+              className="logo-zalo-arc-3"
+              alt="Logo zalo arc"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/logo-zalo-arc-6@2x.png"
+            />
+            <img
+              className="logo-zalo-arc-4"
+              alt="Logo zalo arc"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/logo-zalo-arc-7@2x.png"
+            />
+            <img
+              className="logo-zalo-arc-5"
+              alt="Logo zalo arc"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/logo-zalo-arc-8@2x.png"
+            />
+            <img
+              className="logo-zalo-arc-6"
+              alt="Logo zalo arc"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64e8a20ccf0256120bd2132e/releases/64e8a22e4850be3f0cc9c86f/img/logo-zalo-arc-9@2x.png"
+            />
+          </div>
+          <div className="text-wrapper-38">Top Companies</div>
+        </div>
+      </div>
     </div>
   );
 }
